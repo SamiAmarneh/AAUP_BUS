699f38a0118bb1f66b5986b40bb60ded883f1cf7
Ayman-Yahia <lunayaha092@gmail.com>
2026-04-22 23:31:25 +0300

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,9 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 
-/// From `android/app/google-services.json` (Android package `com.aaup.bus`).
-/// For iOS/macOS/web, add those apps in the Firebase console and run
-/// `flutterfire configure`, or add matching `FirebaseOptions` here.
 class DefaultFirebaseOptions {
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyCzrlynYB2Fu--tP_zWpamiLfIQeWaLD1s',
